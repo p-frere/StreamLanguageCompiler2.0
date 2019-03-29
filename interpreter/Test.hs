fbb71db8336b5a0601d0d31109b6a4fd8ebec895
p-frere <phoebefrere@gmail.com>
2019-03-29 14:29:15 +0000

    fixed for single streams

--- interpreter/Test.hs
+++ interpreter/Test.hs
@@ -5,7 +5,8 @@ import System.Environment
 import Control.Exception
 import System.IO
 import Control.Monad  
-import Data.Char 
+import Data.Char
+import Text.Read
 
 main :: IO ()
 main = catch main' noParse
@@ -16,21 +17,42 @@ main' = do (fileName : _ ) <- getArgs
            let parsedProg = parseCalc (alexScanTokens sourceText)
            putStrLn ("Parsed as: " ++ (show parsedProg))
            let parsedProg1 = updateMeta parsedProg
+           putStrLn ("Parsed as: " ++ (show parsedProg1))
            --
            let past = getPast parsedProg1
            putStrLn ("Inital Past: " ++ (show past))
-           let funcs = evalFunc parsedProg
+           let funcs = evalFunc parsedProg1
            putStrLn ("Funcs: " ++ (show funcs))
-           
-           forever $ do  
-            l <- getLine
-            let parsedL = parseInput (alexScanTokens l)
-            putStrLn ("Input: " ++ (show parsedL))
-            let past1 = evalIn funcs parsedL past
-            --putStrLn ("Evaluated: " ++ (show past1))
-            print (prettyPrint (snd (head past)))
-            hPutStr stdout (prettyPrint (snd (head past)))
+           result <- getUserInputs funcs past 
+           putStrLn (show result)
+        --    forever $ do  
+        --     l <- getLine
+        --     let parsedL = parseInput (alexScanTokens l)
+        --     putStrLn ("Input: " ++ (show parsedL))
+        --     let past1 = evalIn funcs parsedL past1
+        --     putStrLn ("Evaluated: " ++ (show past))
+        --     --print (prettyPrint (snd (head past1)))
+        --     --hPutStr stdout (prettyPrint (snd (head past1)))
 
+getUserInputs :: FuncList -> Past -> IO [Past]
+getUserInputs f p = do 
+    input <- getLine
+    case parseInput1 input of
+        Nothing    -> return []
+        Just a -> do
+            let parsedL = parseInput (alexScanTokens a)
+            let past1 = evalIn f parsedL p
+            moreinputs <- getUserInputs f past1
+            return  (past1 : moreinputs)
+
+parseInput1 :: String -> Maybe String
+--parseInput1 input = if input == "exit" then Nothing else (readMaybe input):: Maybe String
+--parseInput1 input = (Just input):: Maybe String
+parseInput1 input = if input == "exit" then Nothing else (Just input):: Maybe String
+
+
+        
+                
 
 getFuncs :: (Meta,Meta,Meta)  -> [Expr]
 getFuncs (_,_,(MtFuncs fs)) = fs
@@ -40,6 +62,9 @@ prettyPrint :: [Expr] -> String
 prettyPrint ((Grammar.ExInt x):xs) 
     | xs /= [] = show x ++ " " ++ prettyPrint xs
     | ((Grammar.ExInt x):xs) /= []  = show x
+
+
+    
 
 noParse :: ErrorCall -> IO ()
 noParse e = do let err =  show e
